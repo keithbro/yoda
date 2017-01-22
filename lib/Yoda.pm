9d1595e3950b9bce51de5d4292367277b54a5089
Keith Broughton <keithbro256@gmail.com>
2017-01-22 11:45:20 +0000

    added map function

--- lib/Yoda.pm
+++ lib/Yoda.pm
@@ -5,7 +5,32 @@ use warnings;
 
 our $VERSION = "0.01";
 
+=head2 map
 
+    Functor f => (a → b) → f a → f b
+
+=cut
+
+sub map {
+    _curry2(sub {
+        my ($function, $functor) = @_;
+        return [ map { $function->($_) } @$functor ];
+    }, @_);
+}
+
+sub _curry1 { _curry_n(1, @_) }
+sub _curry2 { _curry_n(2, @_) }
+sub _curry3 { _curry_n(3, @_) }
+
+sub _curry_n {
+    my ($arity, $func, @args) = @_;
+
+    if (scalar @args >= $arity) {
+        return $func->(@args);
+    }
+
+    return sub { $func->(@args, @_) };
+}
 
 1;
 __END__
