6f17c37ca1f3cabb15aeb42966dbcb0f6113e54d
Keith Broughton <keithbro256@gmail.com>
2017-01-22 11:39:40 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Yoda - It's new $module
+
+# SYNOPSIS
+
+    use Yoda;
+
+# DESCRIPTION
+
+Yoda is ...
+
+# LICENSE
+
+Copyright (C) Keith Broughton.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Keith Broughton <keithbro256@gmail.com>
---
+++ lib/Yoda.pm
@@ -0,0 +1,39 @@
+package Yoda;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Yoda - It's new $module
+
+=head1 SYNOPSIS
+
+    use Yoda;
+
+=head1 DESCRIPTION
+
+Yoda is ...
+
+=head1 LICENSE
+
+Copyright (C) Keith Broughton.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Keith Broughton E<lt>keithbro256@gmail.comE<gt>
+
+=cut
+
